ee4145af69951bf6345819a1792e37737ff221d4
Wuxiirui <91250646+Wuxiirui@users.noreply.github.com>
2022-01-18 20:18:10 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,14 @@
-// tests go here; this will not be compiled when this package is used as a library
+{
+  // Drive 100 ms forward
+  bitbot.motor(BBMotor.All, 1023);
+  basic.pause(100);
+
+  // Drive 100 ms reverse
+  bitbot.motor(BBMotor.All, -1023);
+  basic.pause(100);
+
+  // Drive 100 ms forward on left and reverse on right
+  bitbot.motor(BBMotor.Left, 1023);
+  bitbot.motor(BBMotor.Right, -1023);
+  basic.pause(100);
+}
