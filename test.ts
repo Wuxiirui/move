b4405655bddef4ebaa2256ac4efd217ce579f22e
Wuxiirui <91250646+Wuxiirui@users.noreply.github.com>
2022-01-19 20:01:20 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,14 +1,4 @@
-{
-  // Drive 100 ms forward
-  bitbot.motor(BBMotor.All, 1023);
-  basic.pause(100);
-
-  // Drive 100 ms reverse
-  bitbot.motor(BBMotor.All, -1023);
-  basic.pause(100);
-
-  // Drive 100 ms forward on left and reverse on right
-  bitbot.motor(BBMotor.Left, 1023);
-  bitbot.motor(BBMotor.Right, -1023);
-  basic.pause(100);
-}
+//tests
+basic.forever(function () {
+    move.forward()
+})
