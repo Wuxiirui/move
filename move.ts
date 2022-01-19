8c163b1782dbe5e0dc5af3f51f937416a12a869a
Wuxiirui <91250646+Wuxiirui@users.noreply.github.com>
2022-01-19 20:00:58 +0800

    Update move.ts

--- move.ts
+++ move.ts
@@ -1,54 +1,19 @@
-namespace play {
+
+
+//% weight=0 color=#0fbc11  icon="\uf207" block="move"
+namespace move {
+  
 
  /**
-      * Move robot forward (or backward) at speed.
-      * @param direction Move Forward or Reverse
-      * @param speed speed of motor between 0 and 100. eg: 60
-      */
-    //% blockId="BBGo" block="go%direction|at speed%speed|\\%"
-    //% speed.min=0 speed.max=100
-    //% weight=100
-    //% subcategory=Motors
-    export function go(direction: BBDirection, speed: number): void
-    {
-        move(BBMotor.Both, direction, speed);
-    }
-
-/**
-      * Stop robot by coasting slowly to a halt or braking
-      * @param mode Brakes on or off
-      */
-    //% blockId="BBstop" block="stop with%mode"
-    //% weight=60
-    //% subcategory=Motors
-    export function stop(mode: BBStopMode): void
-    {
-        getModel();
-        let stopMode = 0;
-        if (mode == BBStopMode.Brake)
-            stopMode = 1;
-        pins.digitalWritePin(lMotorD0, stopMode);
-        pins.digitalWritePin(lMotorD1, stopMode);
-        pins.digitalWritePin(rMotorD0, stopMode);
-        pins.digitalWritePin(rMotorD1, stopMode);
-    }
-
-    function createCalib(speed: number): void
-    {
-        if (getVersionCode() == 5)
-        {        
-            let calibVal = 0;
-            if (speed < 60)
-                calibVal = calibration[1] - ((60 - speed)/30) * (calibration[1] - calibration[0]);
-            else
-                calibVal = calibration[2] - ((90 - speed)/30) * (calibration[2] - calibration[1]);
-            leftCalib = 0;
-            rightCalib = 0;
-            if (calibVal < 0)
-                leftCalib = Math.abs(calibVal);
-            else
-                rightCalib = calibVal;
-        }
-    }
-
+    *MOVE: full speed move forward
+    */
+    //% weight=9
+    //% blockId=move_forward block="forword"
+    export function forword():  {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P3, 512)
+    pins.analogWritePin(AnalogPin.P4, 512) 
+}
+  
 }
